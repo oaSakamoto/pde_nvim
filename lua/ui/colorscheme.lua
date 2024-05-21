e79beae22b81e2e1332657c4ccf890af21aa94f4
Olimpio Sakamoto <oasakamoto@proton.me>
2024-05-21 17:35:20 -0300

    feat: adiciona highligth pata variaveis

--- lua/ui/colorscheme.lua
+++ lua/ui/colorscheme.lua
@@ -6,7 +6,10 @@ return {
 		priority = 1000,
 		flavor = "latte",
 		config = function()
+			local colors = require("catppuccin.palettes").get_palette("mocha")
 			vim.cmd([[colorscheme catppuccin-mocha]])
+			-- Redefine a cor para variaveis
+			vim.api.nvim_set_hl(0, "@variable", { fg = colors.teal })
 		end,
 	},
 }
