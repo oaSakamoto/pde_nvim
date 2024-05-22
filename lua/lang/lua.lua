4dc932ebbb68023133749ce8be619e33627f5f8a
Olimpio Sakamoto <oasakamoto@proton.me>
2024-05-21 23:35:01 -0300

    feat: adiciona e configura lua_ls

--- lua/lang/lua.lua
+++ lua/lang/lua.lua
@@ -6,4 +6,10 @@ return {
 			vim.list_extend(opts.ensure_installed, { "lua", "luadoc", "luap" })
 		end,
 	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		opts = function(_, opts)
+			vim.list_extend(opts.ensure_installed, { "lua_ls" })
+		end,
+	},
 }
