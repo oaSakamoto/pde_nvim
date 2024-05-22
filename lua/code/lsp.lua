1e15994f6bff0120545c4a3e22fa5db36e139cb0
Olimpio Sakamoto <oasakamoto@proton.me>
2024-05-21 23:33:52 -0300

    feat: adiciona e configura lsp com lsp-mason

---
+++ lua/code/lsp.lua
@@ -0,0 +1,40 @@
+return {
+	"williamboman/mason-lspconfig.nvim",
+	lazy = false,
+	dependencies = {
+		{
+			"williamboman/mason.nvim",
+			lazy = false,
+		},
+		{
+			"neovim/nvim-lspconfig",
+			lazy = false,
+		},
+		{
+			"folke/neodev.nvim",
+			lazy = false,
+			opts = {},
+			config = function()
+				require("neodev").setup({})
+			end,
+		},
+	},
+	opts = {
+		ensure_installed = { "rust_analyzer" },
+		handlers = {
+			function(server_name)
+				require("lspconfig")[server_name].setup({})
+			end,
+		},
+	},
+
+	config = function(_, opts)
+		require("mason").setup()
+
+		require("mason-lspconfig").setup({
+			ensure_installed = opts.ensure_installed,
+			automatic_installation = true,
+		})
+		require("mason-lspconfig").setup_handlers(opts.handlers)
+	end,
+}
