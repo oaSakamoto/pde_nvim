6768c05af43598945fe9a82ebeddad5996705db2
Olimpio Sakamoto <oasakamoto@proton.me>
2024-05-22 17:00:36 -0300

    feat: adiciona configuracao lua_ls

--- lua/lang/lua.lua
+++ lua/lang/lua.lua
@@ -1,15 +1,43 @@
 return {
-	{
-		"nvim-treesitter/nvim-treesitter",
-		lazy = false,
-		opts = function(_, opts)
-			vim.list_extend(opts.ensure_installed, { "lua", "luadoc", "luap" })
-		end,
-	},
-	{
-		"williamboman/mason-lspconfig.nvim",
-		opts = function(_, opts)
-			vim.list_extend(opts.ensure_installed, { "lua_ls" })
-		end,
-	},
+  {
+    "nvim-treesitter/nvim-treesitter",
+    lazy = false,
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, { "lua", "luadoc", "luap" })
+    end,
+  },
+  {
+    "williamboman/mason-lspconfig.nvim",
+    lazy = false,
+    dependencies = {
+      "folke/neodev.nvim",
+      opts = {},
+    },
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, { "lua_ls" })
+      local capabilities = vim.lsp.protocol.make_client_capabilities()
+      capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
+
+      opts.handlers = vim.tbl_deep_extend("force",opts.handlers, {
+        ["lua_ls"]=function ()
+          require("neodev").setup()
+          local lspconfig = require("lspconfig")
+
+          local configs = require("lspconfig.configs")
+          vim.print(configs["lua_ls"])
+          lspconfig.lua_ls.setup({
+            capabilities = capabilities,
+            settings = {
+              Lua = {
+                completion = {
+                  callSnippet = "Replace",
+                  displayCotext = 4,
+                },
+              },
+            },
+          })
+        end,
+      })
+    end,
+  },
 }
