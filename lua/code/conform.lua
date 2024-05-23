8c9eebce68fb52937018b4b3d8e94e19e02ffd8c
Olimpio Sakamoto <oasakamoto@proton.me>
2024-05-23 13:47:23 -0300

    feat: configura plugin para formatar arquivos
    conform configurado para lua e bash com stylua e shfmt

---
+++ lua/code/conform.lua
@@ -0,0 +1,40 @@
+return {
+	"stevearc/conform.nvim",
+	dependencies = {
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		lazy = false,
+		config = function()
+			require("mason-tool-installer").setup({
+				ensure_installed = {
+					"stylua",
+					"shfmt",
+				},
+			})
+		end,
+	},
+	event = { "BufWritePre" },
+	cmd = { "ConformInfo" },
+	keys = {
+		{
+			-- Customize or remove this keymap to your liking
+			"<leader>f",
+			function()
+				require("conform").format({ async = true, lsp_fallback = true })
+			end,
+			mode = "",
+			desc = "Format buffer",
+		},
+	},
+	-- Everything in opts will be passed to setup()
+	opts = {
+		-- Define your formatters
+		formatters_by_ft = {
+			lua = { "stylua" },
+			sh = { "shfmt" },
+			-- python = { "isort", "black" },
+			-- javascript = { { "prettierd", "prettier" } },
+		},
+		-- Set up format-on-save
+		-- format_on_save = { timeout_ms = 500, lsp_fallback = true },
+	},
+}
