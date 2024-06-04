bf294b65adcd54f7af82f1c7a818877491f7d02e
Olimpio Sakamoto <oasakamoto@proton.me>
2024-06-04 14:38:41 -0300

    feat: adiciona config para o filetype xkb

--- init.lua
+++ init.lua
@@ -2,5 +2,11 @@ require("config.options")
 require("config.lazy")
 
 vim.api.nvim_create_autocmd("UIEnter", {
-  command = "Telescope file_browser",
+	command = "Telescope file_browser",
+})
+
+vim.filetype.add({
+	filename = {
+		["~/personal/parses_tressitter/tree-sitter-xkb/examples/dvorak"] = "xkb",
+	},
 })
